c7bb301e2c66d0f4a71897b386bb14ebbdd3cb1d
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-06 17:29:53 -0500

    Actualizar herramientas-hd.js

--- plugins/herramientas-hd.js
+++ plugins/herramientas-hd.js
@@ -1,80 +1,38 @@
-import FormData from "form-data"
-import Jimp from "jimp"
+import axios from 'axios';
+import FormData from 'form-data';
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-  conn.hdr = conn.hdr ? conn.hdr : {}
-  if (m.sender in conn.hdr)
-    throw m.reply("🤌🏻 Aún hay procesos en el chat >//<");
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || q.mediaType || ""
-  if (!mime)
-    throw m.reply(`*🐈 Etiqueta una Imagen.*`)
-  if (!/image\/(jpe?g|png)/.test(mime))
-    throw m.reply(`*🐈 Etiqueta una Imagen.*`);
-  else conn.hdr[m.sender] = true;
-  m.reply("*Mejorando la calidad de imagen...*")
-  let img = await q.download?.()
-  let error
-  try {
-    const This = await processing(img, "enhance")
-    conn.sendFile(m.chat, This, "", "*Listo :3*", m)
-  } catch (er) {
-    error = true
-  } finally {
-    if (error) {
-      m.reply("Error :(")
-    }
-    delete conn.hdr[m.sender]
-  }
-}
-
-handler.help = ['hd *<img>*', 'remini *<img>*']
-handler.tags = ['herramientas']
-handler.command = /^(hd|remini)$/i
-
-export default handler
-
-async function processing(urlPath, method) {
-  return new Promise(async (resolve, reject) => {
-    let Methods = ["enhance"]
-    Methods.includes(method) ? (method = method) : (method = Methods[0]);
-    let buffer,
-      Form = new FormData(),
-      scheme = "https" + "://" + "inferenceengine" + ".vyro" + ".ai/" + method;
-    Form.append("model_version", 1, {
-      "Content-Transfer-Encoding": "binary",
-      contentType: "multipart/form-data; charset=uttf-8",
-    });
-    Form.append("image", Buffer.from(urlPath), {
-      filename: "enhance_image_body.jpg",
-      contentType: "image/jpeg",
-    });
-    Form.submit(
-      {
-        url: scheme,
-        host: "inferenceengine" + ".vyro" + ".ai",
-        path: "/" + method,
-        protocol: "https:",
-        headers: {
-          "User-Agent": "okhttp/4.9.3",
-          Connection: "Keep-Alive",
-          "Accept-Encoding": "gzip",
-        },
-      },
-      function (err, res) {
-        if (err) reject();
-        let data = [];
-        res
-          .on("data", function (chunk, resp) {
-            data.push(chunk);
-          })
-          .on("end", () => {
-            resolve(Buffer.concat(data));
-          });
-        res.on("error", (e) => {
-          reject();
-        });
-      }
-    );
+async function Uguu(buffer, filename) {
+  let form = new FormData();
+  form.append('files[]', buffer, { filename });
+  let { data } = await axios.post('https://uguu.se/upload.php', form, {
+    headers: form.getHeaders()
   });
+  if (data?.files?.[0]?.url) return data.files[0].url;
+  else throw m.reply('Error al subir imagen.');
 }
+
+let handler = async (m, { conn, args }) => {
+  try {
+    await m.react('⌛');
+    let type = (args[0] === 'mild') ? 'mild' : 'unblur';
+    let q = m.quoted ? m.quoted : m;
+    let mime = (q.msg || q).mimetype || '';
+    if (!mime.startsWith('image/')) throw m.reply('Responde a una imagen.');
+    let media = await q.download();
+    let url = await Uguu(media, `image.${mime.split('/')[1]}`);
+    let { data } = await axios.get(`https://abella.icu/unblur?url=${encodeURIComponent(url)}&type=${type}`, {
+      responseType: 'arraybuffer'
+    });
+    await m.react('✅');
+    await conn.sendMessage(m.chat, { image: Buffer.from(data) }, { quoted: m });
+  } catch (e) {
+    await m.react('❌');
+    m.reply(`${e}`);
+  }
+};
+
+handler.help = ['unblur', 'unblur mild'];
+handler.tags = ['tools'];
+handler.command = ['hd'];
+
+export default handler;
