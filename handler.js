6f4817a01be712ccfbe36a73f405abf54872e494
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-06 20:12:32 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -498,30 +498,37 @@ this.copyNForward(msg.chat, msg).catch(e => console.log(e, msg))
 } catch (e) {
 console.error(e)
 }}*/
-export async function deleteUpdate(message) {
+export async function deleteUpdate(message, conn) {
     try {
         const { fromMe, id, participant } = message
-        if (fromMe)
-            return
-        let msg = conn.serializeM(conn.loadMessage(id))
-        if (!msg)
-            return
-        let chat = global.db.data.chats[id] || {}
-    
-        if (chat.delete) return
-        
-        await conn.reply(msg.chat, `
-Terdeteksi @${participant.split`@`[0]} telah menghapus pesan barusan
-Untuk mematikan fitur ini, ketik
-*.disable antidelete*
-`.trim(), msg, {
+        if (fromMe) return
+
+        const msg = conn.serializeM(conn.loadMessage(id))
+        if (!msg) return
+
+        const chat = global.db.data.chats[msg.chat] || {}
+        if (!chat?.delete) return
+        if (!msg?.isGroup) return
+
+        const antideleteMessage = `
+╭•┈•〘❌ 𝗔𝗡𝗧𝗜 𝗗𝗘𝗟𝗘𝗧𝗘 ❌〙•┈• ◊
+│❒ 𝗨𝗦𝗨𝗔𝗥𝗜𝗢:
+│• @${participant.split`@`[0]}
+│
+│❒ 𝗔𝗰𝗮𝗯𝗮 𝗱𝗲 𝗲𝗹𝗶𝗺𝗶𝗻𝗮𝗿 𝘂𝗻 𝗺𝗲𝗻𝘀𝗮𝗷𝗲
+│𝗿𝗲𝗲𝗻𝘃𝗶𝗮𝗻𝗱𝗼... ⏱️
+╰•┈•〘❌ 𝗔𝗡𝗧𝗜 𝗗𝗘𝗟𝗘𝗧𝗘 ❌〙•┈• ◊`.trim()
+
+        await conn.reply(msg.chat, antideleteMessage, msg, {
             mentions: [participant]
         })
+
         conn.copyNForward(msg.chat, msg).catch(e => console.log(e, msg))
     } catch (e) {
         console.error(e)
     }
 }
+
 
 global.dfail = (type, m, conn) => {
 
